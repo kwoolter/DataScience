96d93b85ad9945d8c330bfde1bc826108dfb7266
kwoolter <keith.woolterton@gmail.com>
2020-05-13 18:41:55 +0800

    synched state transitions

--- data_science/pandas1.py
+++ data_science/pandas1.py
@@ -4,7 +4,7 @@ from .kwutils import *
 
 import os
 
-DATA_FILES_DIR = os.path.join(os.path.dirname(__file__),"data")
+DATA_FILES_DIR = os.path.join(os.path.dirname(__file__),"FSM")
 
 def df_test():
 
@@ -31,7 +31,7 @@ def df_test():
 def df_test2():
 
     """ Use state transition data files and navigate them with pandas"""
-    title = "Welcome to the finite state machine game!"
+    title = "Welcome to the finite state machine game!".title()
     print("*" * (len(title) + 5))
     print("* " + title + " *")
     print("*" * (len(title) + 4))
@@ -68,21 +68,21 @@ def df_test2():
 
         # Print the details of the current state
         current_state = states.loc[current_state_id].to_dict()
-        print(f'{current_state["Name"]} : {current_state["Description"]}')
+        print(f'{current_state["Name"].title()} : {current_state["Description"].capitalize()}')
 
         # If now is a good time to display the scores then do so
         if current_state_id in score_states:
             print("Scores:")
             for key,value in scores.items():
-                print(f'\t{key}:{value}')
+                print(f'\t{key.title()}:{value}')
 
-        # If the current state has not transitions then end
+        # If the current state has no transitions then end
         if current_state_id not in state_transitions.index:
             print(f'No choices available for state {current_state["Name"]}')
             break
 
         # Get the list of transitions for the current state
-        #transitions = state_transitions.loc[[current_state_id]]
+        # Not using .loc() as can return Series or DataFrame in certain scenarios
         transitions = state_transitions[state_transitions.index == current_state_id]
 
         try:
@@ -92,19 +92,22 @@ def df_test2():
             print("Bye Bye")
             break
 
-
-        # Get the state transition details for teh selected state
+        # Get the state transition details for the chosen state
         transition = transitions[(transitions["Input"] == choice)].to_dict(orient="index")[current_state_id]
 
-        # Process the state transition
-        print(transition["Output"])
+        # Process the state transition using the dictionary data
+        print(transition["Output"].capitalize())
         current_state_id = transition["Next State"]
-
         for score_name in score_names:
             scores[score_name] += transition[score_name]
 
         # User presses enter then loop
         x = input()
+
+    # See how you got on in the game...
+    print("\nFinal Scores:")
+    for key, value in scores.items():
+        print(f'\t{key.title()}:{value}')
 
 
 def df_test3():
